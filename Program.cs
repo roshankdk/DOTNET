43f4cb08c860f526d1f6ceeb4298401c66c798c8
roshankdk <roshankdk.27@gmail.com>
2024-11-19 07:21:08 +0545

    changes in programs

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ class Program
 
         for (int i = 0; i <= 5; i++)
         {
-            Console.Write(num[i] + " ");
+            Console.Write(num[i] + " " + "roshan");
         }
 
         foreach(int i in num){
